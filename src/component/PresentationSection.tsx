0ee5c4326a203fc2ed664e240d50cfc4d82eada5
Louis Aussedat <aussedat.louis@gmail.com>
2022-04-27 22:01:24 +0200

    Presentation: add parallax effect

--- src/component/PresentationSection.tsx
+++ src/component/PresentationSection.tsx
@@ -1,20 +1,32 @@
 import { Box, Button, Grid, styled, Typography } from "@mui/material";
+import { Parallax } from "react-parallax";
 import ElvateText from "src/assets/elvate-text.svg";
+import ElvateIcon from "src/assets/elvate-icon.svg";
 import BarSvg from "src/component/BarSvg";
 
 const Root = styled(Box)`
-  justify-content: center;
+  padding-bottom: ${({ theme }) => theme.spacing(15)};
+  margin-top: ${({ theme }) => theme.spacing(5)};
+  justify-content: flex-end;
+  flex-direction: column;
   align-items: center;
   display: flex;
   height: 90vh;
-  margin-top: 5em;
-  flex-direction: column;
-  align-content: space-between;
-  position: relative;
+
+  @media (max-width: 800px) {
+    padding-bottom: ${({ theme }) => theme.spacing(8)};
+  }
+
+  @media (max-width: 599px) {
+    padding-bottom: ${({ theme }) => theme.spacing(5)};
+    height: 100vh;
+  }
 `;
 
-const ElvateTextImg = styled("img")`
-  height: 40px;
+const Container = styled(Grid)`
+  padding-right: ${({ theme }) => theme.spacing(5)};
+  padding-left: ${({ theme }) => theme.spacing(5)};
+  max-width: 75vh;
 `;
 
 const ButtonItem = styled(Grid)`
@@ -25,47 +37,64 @@ const ButtonItem = styled(Grid)`
 
 const PresentationSection = () => {
   return (
-    <Root>
-      <Box>
-        <Grid
-          container
-          columns={{ xs: 1, sm: 2 }}
-          paddingLeft={5}
-          paddingRight={5}
-        >
-          <Grid item xs={1} sm={2} padding={1}>
-            <ElvateTextImg src={ElvateText} />
-            <Typography>
-              A decentralized application to perform DCA strategy.
-            </Typography>
-          </Grid>
-          <ButtonItem item xs={1} padding={1} paddingTop={{ xs: 3, sm: 1 }}>
-            <Button
-              variant="contained"
-              onClick={() => {
-                window.open("https://app.elvate.io");
-              }}
-              fullWidth
-            >
-              Open Application
-            </Button>
-          </ButtonItem>
-          <ButtonItem item xs={1} padding={1}>
-            <Button
-              variant="outlined"
-              onClick={() => {
-                window.open("https://docs.elvate.io");
-              }}
-              fullWidth
-            >
-              Documentation
-            </Button>
-          </ButtonItem>
-        </Grid>
-      </Box>
-
-      <BarSvg transform="scale(-1,1)" sx={{ bottom: 0 }} />
-    </Root>
+    <Parallax
+      blur={{ min: -5, max: 5 }}
+      bgImage={ElvateText}
+      bgImageStyle={{
+        height: "80vh",
+        minWidth: 250,
+        width: "30%",
+        maxWidth: 500,
+      }}
+      strength={300}
+    >
+      <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={ElvateIcon}
+        bgImageStyle={{
+          height: "80vh",
+          minWidth: 200,
+          maxWidth: 500,
+          width: "20%",
+          zIndex: -1,
+          opacity: 0.1,
+        }}
+        strength={500}
+      >
+        <Root>
+          <Container container columns={{ xs: 1, sm: 2 }}>
+            <Grid item xs={1} sm={2} padding={1} textAlign="center">
+              <Typography variant="h6">
+                An Opensource Decentralized Application to perform DCA strategy.
+              </Typography>
+            </Grid>
+            <ButtonItem item xs={1} padding={1} paddingTop={{ xs: 3, sm: 1 }}>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  window.open("https://app.elvate.io");
+                }}
+                fullWidth
+              >
+                Open Application
+              </Button>
+            </ButtonItem>
+            <ButtonItem item xs={1} padding={1}>
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  window.open("https://docs.elvate.io");
+                }}
+                fullWidth
+              >
+                Documentation
+              </Button>
+            </ButtonItem>
+          </Container>
+          <BarSvg transform="scale(-1,1)" sx={{ bottom: -1 }} />
+        </Root>
+      </Parallax>
+    </Parallax>
   );
 };
 
